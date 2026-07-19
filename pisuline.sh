e0dd73e03c388a2d1aabb4296d1d0dae1f22c53b
Lauren Toivanen <lauren@lauren.ax>
2026-07-19 23:18:27 +0300

    Gracefully notify if pisuline is not installed

--- pisuline.sh
+++ pisuline.sh
@@ -5,5 +5,11 @@
 function ps1_pisuline() {
     PS1="$(pisuline 1 $?)\n"
 }
-PROMPT_COMMAND="ps1_pisuline"
-PS0='$(pisuline 0)'
+
+# check if pisuline is installed
+if command -v pisuline >/dev/null 2>&1; then
+    PROMPT_COMMAND="ps1_pisuline"
+    PS0='$(pisuline 0)'
+else
+    echo "pisuline.sh: Pisuline is not installed."
+fi
